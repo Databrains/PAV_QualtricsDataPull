e0ea1b5612febcccc86bf3f6993f3f643776016a
databrains01JM <42188621+databrains01JM@users.noreply.github.com>
2019-01-18 14:44:32 -0500

    Update parseSurveyOutput.py
    Removed questions that don't need to be pivoted and added comments to make code more readable.

--- parseSurveyOutput.py
+++ parseSurveyOutput.py
@@ -2,33 +2,39 @@ import csv
 import json
 from  datetime import datetime as dt
 
+
+'''****************************************************
+    Set Up CSV Files
+****************************************************'''
 runDate = dt.utcnow().strftime('%Y.%m.%d.T.%H.%M.%S')
 # jsonFileName = r'C:\Users\JefferyMcCain\Documents\Consulting\Athlete View\DataDumps\OutputFiles\surveyOutput_' + runDate
 csvFileName = r'C:\Users\JefferyMcCain\Documents\Consulting\Athlete View\DataDumps\OutputFiles\SA Core Survey.csv'
 outputPivotFile = r'C:\Users\JefferyMcCain\Documents\Consulting\Athlete View\DataDumps\OutputFiles\PivotedSurveyResults_' + runDate + ".txt"
+doNotPivotFile = r'C:\Users\JefferyMcCain\Documents\Consulting\Athlete View\DoNotPivotQuestions.csv'
 
-# file = pd.read_csv(csvFileName, skiprows=range(1,3))
-# pivot = file.pivot(index='ResponseID', columns='Q5', )
-#
-# csvFile = open(, 'r', encoding="utf8")
-# jsonFile = open(jsonFileName, 'w')
-# # reader = csv.DictReader(csvFile)
-# # for row in reader:
-# #     json.dump(row, jsonFile)
-# #     jsonFile.write('\n')
-# #
-# # print(jsonFile)
-#
-#
-#
-# print(file.head(5))
-# print(pivot)
 
+'''***************************************************
+    Set Up File to be written
+***************************************************'''
 writer=csv.writer(open(outputPivotFile,'a',encoding='utf8'),delimiter="|")
 header = ['ResponseID', 'Question', 'Answer' ]
 rowCount = 1
 csvFile = open(csvFileName, 'r',encoding='utf8')
 
+
+'''***************************************************
+    Create List of Questions to not pivot
+***************************************************'''
+doNotPivot = []
+with open(doNotPivotFile) as file:
+        reader = csv.reader(file, delimiter=",")
+        for row in reader:
+            doNotPivot.append(row[0])
+
+
+'''**************************************************
+        Write Pivoted Output File
+**************************************************'''
 reader = csv.DictReader(csvFile)
 print(reader.fieldnames)
 writer.writerow(header)
@@ -40,8 +46,9 @@ for row in reader:
         if listOfItems[0] > 3:
             if listOfItems[3] != "":
                 surveyResults = listOfItems[1:]
-                print(listOfItems[1:])
-                writer.writerow(surveyResults)
+                if surveyResults[1] not in doNotPivot:
+                    print(listOfItems[1:])
+                    writer.writerow(surveyResults)
 
 
 
